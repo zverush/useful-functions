a3a86480169ed02bb21883dd014f5e7805885933
Aleynikov Sergey <topblogs.su@gmail.com>
2014-03-21 11:40:29 +0300

    Create yii-image.php

---
+++ yii/yii-image.php
@@ -0,0 +1,20 @@
+<?php
+  
+  /*
+  
+  CImageHandler - работа с изображениями
+  http://yiiframework.ru/forum/viewtopic.php?f=9&t=882
+  https://github.com/tokolist/yii-components/blob/master/protected/extensions/yiicomp/components/ImageHandler/readme_ru.md
+  
+  
+  Convert text from your database into an image using ImageMagick
+  http://www.yiiframework.com/forum/index.php/topic/27946-convert-text-from-your-database-into-an-image-using-imagemagick/
+  
+  
+  text-field-img
+  Create image text field helping prevent information crawing bot.
+  http://www.yiiframework.com/extension/text-field-img/
+  
+  */
+  
+?>

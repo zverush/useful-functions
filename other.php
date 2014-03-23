d0e706ceb499f8bc2341ebd7f0cfb60040d57cc8
Aleynikov Sergey <topblogs.su@gmail.com>
2014-03-23 12:30:39 +0300

    Update other.php
    fix variable name

--- other.php
+++ other.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Возвращает форму слова в зависимости от колличества $count.
+ * Возвращает форму слова в зависимости от колличества $number.
  * 
- * @param int $count <p>число элементов</p>
+ * @param int $number <p>число элементов</p>
  * @param array $forms <p>Формы слова для количества 1, 2 и 5, пример {носок,носка,носков}</p>
  */
-function GetWordForms($count,$forms)
+function GetWordForms($number,$forms)
 {
     $cases = array (2, 0, 1, 1, 1, 2);
     return $forms[ ($number%100 >4 && $number%100< 20)? 2 : $cases[min($number%10, 5)] ];

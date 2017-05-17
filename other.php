0e911a89292574acf39261378eb8d91921de30e2
Aleynikov Sergey <topblogs.su@gmail.com>
2017-05-17 14:47:37 +0300

    Update other.php

--- other.php
+++ other.php
@@ -2,14 +2,12 @@
 
 /**
  * Возвращает форму слова в зависимости от колличества $number.
- * 
+ *
  * @param int $number <p>число элементов</p>
  * @param array $forms <p>Формы слова для количества 1, 2 и 5, пример {носок,носка,носков}</p>
+ * @return string
  */
-function GetWordForms($number,$forms)
-{
+function get_word_form( $number,$forms ) {
     $cases = array (2, 0, 1, 1, 1, 2);
     return $forms[ ($number%100 >4 && $number%100< 20)? 2 : $cases[min($number%10, 5)] ];
-};
-
-?>
+}
